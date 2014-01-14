cb41c493d892bd16db8c7708b9446ce9cbc7a6d4
Majiir Paktu <majiir@nabaal.net>
2014-01-14 16:39:29 -0500

    Wrapped function is always called with expected+1 arguments

--- safesocket.js
+++ safesocket.js
@@ -8,22 +8,27 @@ module.exports = exports = safesocket;
  * Wraps socket.io event handlers to prevent crashes from certain malicious inputs.
  */
 
-function safesocket (fn) {
+function safesocket (expected, fn) {
 	return function () {
 		var _args = [];
 		for (var idx in arguments) {
 			_args.push(arguments[idx]);
 		}
 
-		var args = [_args];
-		if (_args[_args.length - 1] instanceof Function) {
-			args.push(_args.pop());
-		} else {
-			args.push(noop);
+		var args = _args.filter(notFunction).slice(0, expected);
+		for (var i = args.length; i < expected; i++) {
+			args.push(undefined);
 		}
+
+		var arg = _args.pop();
+		args.push((arg instanceof Function) ? arg : noop);
 
 		fn.apply(this, args);
 	};
 }
 
 function noop () {}
+
+function notFunction (fn) {
+	return !(fn instanceof Function);
+}
